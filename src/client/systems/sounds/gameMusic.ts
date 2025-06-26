cd0b5548f427ea4fd3829eb8dee42a296fc58a4c
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2025-06-26 15:43:34 -0400

    fixed sound track

--- src/client/systems/sounds/gameMusic.ts
+++ src/client/systems/sounds/gameMusic.ts
@@ -7,31 +7,26 @@ import paths from "shared/utils/paths";
 
 
 let chosenTrack: GameMusicNames | undefined = undefined;
+let playingTrack: number = 0;
 
 // tells you if the track has been completed
 function trackCompleted() {
     const trackFolder = chosenTrack && paths.SFX.GameMusic[chosenTrack]
-    return !trackFolder || !trackFolder.FindFirstChild<Sound>("Sound" + trackFolder.GetChildren().size())?.IsPlaying;
+    return !trackFolder || playingTrack > trackFolder.GetChildren().size()
 }
 
 export default (world: World) => {
+    const trackFolder = chosenTrack && paths.SFX.GameMusic[chosenTrack]
+    const playingSound = trackFolder && trackFolder.FindFirstChild<Sound>("Sound" + playingTrack);
     if (trackCompleted()) {
         const trackFoldersToChoseFrom = paths.SFX.GameMusic.GetChildren().filter((child) => child.Name !== chosenTrack)
         const folderToPlayMusicFrom = trackFoldersToChoseFrom[math.floor(trackFoldersToChoseFrom.size() * math.random())] as Folder;
 
         // sets the new chosen track
         chosenTrack = folderToPlayMusicFrom.Name as GameMusicNames;
-
-        // plays through all the tracks
-        task.spawn(() => {
-            for (let i = 1; i <= folderToPlayMusicFrom.GetChildren().size(); i++) {
-                const sound = folderToPlayMusicFrom.FindFirstChild<Sound>("Sound" + i);
-                if (sound) {
-                    sound.Playing = false;
-                    sound.Play();
-                    sound.Stopped.Wait()
-                }
-            }
-        })
+        playingTrack = 0;
+    } else if (!playingSound?.IsPlaying && trackFolder) {
+        playingTrack += 1;
+        trackFolder.FindFirstChild<Sound>("Sound" + playingTrack)?.Play();
     }
 }
