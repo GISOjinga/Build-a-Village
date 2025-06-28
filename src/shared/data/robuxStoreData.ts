3ace58560dceae91b4d836f104f6a47f21538a66
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2025-06-28 13:52:44 -0400

    tweening coins

--- src/shared/data/robuxStoreData.ts
+++ src/shared/data/robuxStoreData.ts
@@ -7,14 +7,14 @@ const robuxStoreData: RobuxStoreData = {
     StarterPack: {
         Name: "Starter Pack",
         TimeEnds: os.time() + 24 * 60 * 60,
-        ProductId: 1285438020,
+        ProductId: 3319212062,
         Coins: 1000,
         Villagers: ["Farmer", "Miner", "Baker"],
     },
     LaunchPack: {
         Name: "Launch Pack",
         TimeEnds: os.time() + 7 * 24 * 60 * 60,
-        ProductId: 1283646112,
+        ProductId: 3319212063,
         Villagers: ["Carpenter", "Mason", "Tailor", "Blacksmith", "Blacksmith"],
     },
 };
