55fd41bd99f192cee18f8eff05ea06ddec13b101
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2025-07-04 14:31:53 -0400

    Use LogCustomEvent for analytics

--- src/server/utils/analytics.ts
+++ src/server/utils/analytics.ts
@@ -55,7 +55,7 @@ export enum GameEvent {
     TutorialAbandoned = "tutorial_abandoned",
 }
 
-export function logGameEvent(player: Player | undefined, event: GameEvent, data?: unknown) {
-    printTS($line, `Logging event: ${event} for player: ${player?.Name ?? "Server"}`);
-    AnalyticsService.FireCustomEvent(player, event, data);
+export function logGameEvent(player: Player, event: GameEvent, data?: unknown) {
+    printTS($line, `Logging event: ${event} for player: ${player.Name}`);
+    AnalyticsService.LogCustomEvent(player, event, undefined, data);
 }
